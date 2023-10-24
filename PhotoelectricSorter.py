58d48f75e6e13c65ae7e222ac538b63b5803972c
XavierBurt <rxb@hawaii.edu>
2023-10-23 16:25:47 -1000

    Framework made

--- PhotoelectricSorter.py
+++ PhotoelectricSorter.py
@@ -1,20 +1,22 @@
 # This is a sample Python script.
+import numpy as np
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# These variables are inputted from the command line
+temp = 0  # Temperature (in kelvin)
+wavelength = 0  # Wavelength (in meters)
 
+# Their original values have no meaning, I'm just not good enough with python to not init them.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+WF_database = pd.read_csv('WF_database_29270_part1.csv')
+WF_database2 = pd.read_csv('WF_database_29270_part2.csv')
 
+wf = 0  # work function
+matName = 'AlCl'  # name of the material
+print(len(WF_database.columns))
+print(WF_database.size / len(WF_database.columns))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for row in WF_database.itertuples(index=False):
+    matName = row.surface_elements_string
+    wf = row.WF
+    print(f'{matName}: {wf}')

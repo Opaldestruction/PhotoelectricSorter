b30fce7e11f590280f6ce29976d4cc1ee61178f7
XavierBurt <rxb@hawaii.edu>
2023-11-06 21:08:24 -1000

    Sorted

--- PhotoelectricSorter.py
+++ PhotoelectricSorter.py
@@ -12,8 +12,15 @@ wavelength = 0  # Wavelength (in meters)
 
 # Their original values have no meaning, I'm just not good enough with python to not init them.
 
-WF_database = pd.read_csv('WF_database_29270_part1.csv')
+WF_database1 = pd.read_csv('WF_database_29270_part1.csv')
 WF_database2 = pd.read_csv('WF_database_29270_part2.csv')
+
+# TODO: Make it so that we can actually combine the two databases in a clean way.
+#  (where the graphs don't look unreadable.)
+
+# IMPORTANT CHANGE THIS IF YOU WANT TO SWITCH INPUTS.
+
+WF_database = WF_database2
 
 aluminums, aluminumsH, aluminumsErr = [], [], []
 zincs, zincsH, zincsErr = [], [], []
@@ -35,23 +42,29 @@ for row in WF_database.itertuples(index=False):
     matName = row.surface_elements_string
     if "Al" in matName:
         utils.duplicate_averager(aluminums, aluminumsH, matName, row, savedMatNameAl, numAluminums)
+        utils.multiple_sort(aluminumsH, [aluminumsH, aluminums])
         # utils.duplicate_stderr(aluminums, aluminumsH, aluminumsErr, matName, row, savedMatNameAl, numAluminums)
     if "Zn" in matName:
         utils.duplicate_averager(zincs, zincsH, matName, row, savedMatNameZn, numZincs)
-
+        utils.multiple_sort(zincsH, [zincsH, zincs])
     if "Mn" in matName:
         utils.duplicate_averager(magnesiums, magnesiumsH, matName, row, savedMatNameMn, numMagnesiums)
-
+        utils.multiple_sort(magnesiumsH, [magnesiumsH, magnesiums])
     if "Si" in matName:
         utils.duplicate_averager(silicons, siliconsH, matName, row, savedMatNameSi, numSilicons)
-
+        utils.multiple_sort(siliconsH, [siliconsH, silicons])
     if "Ti" in matName:
         utils.duplicate_averager(titaniums, titaniumsH, matName, row, savedMatNameTi, numTitaniums)
+        utils.multiple_sort(titaniumsH, [titaniumsH, titaniums])
+
+# BELOW IS THE SECTION FOR SHOWING THE GRAPHS
+# TODO: MAKE IT LOOK BETTER
 
 # gray = Color("#a2b1ae")  # defining gray
 # colors = list(gray.range_to(Color("#010101"), aluminums.__len__()))  # Color is a gradient from gray to white
 # for i, color in enumerate(colors):  # make colors a list of strings
 #     colors[i] = color.hex
+
 alX = (range(len(aluminums)))
 alX = [2 * i for i in alX]
 fig, al = plt.subplots(figsize=(20, 15))
@@ -92,3 +105,4 @@ plt.xticks(rotation='vertical', fontsize=5)
 ti.set_ylabel('Work Function')
 ti.set_title('Titanium compounds and their work functions')
 plt.show()
+
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import copy
 import math
 
 
@@ -23,6 +24,7 @@ def duplicate_averager(x_list, y_list, material_name, row, saved_mat_name, num_d
             num_duplicates = 1
 
 
+# TODO: I haven't finished this yet, I'm hoping to, as I think it could give as good an insight as the WF itself.
 def duplicate_stderr(x_list, y_list, err_list, material_name, row, saved_mat_name, num_duplicates):
     err = 0
     temp = 0
@@ -47,3 +49,71 @@ def duplicate_stderr(x_list, y_list, err_list, material_name, row, saved_mat_nam
             saved_mat_name = ''
             num_duplicates = 1
     print(err_list)
+
+
+# If you have any optimizations for my "multiple_sort" feel free to add them below.
+def multiple_sort(list_1, list_of_lists):
+    for list_i in list_of_lists:
+        if not list_i.__len__() == list_1.__len__():
+            print("you stup*d. can't sort lists with different lengths. dum*a**")
+            return
+        # Find the maximum number to know number of digits
+
+    # forgot floats existed, casting to integers
+    for i in range(0, len(list_1)):
+        list_1[i] = int(list_1[i] * 100)
+    max1 = max(list_1)
+
+    # Do counting sort for every digit. Note that instead
+    # of passing digit number, exp is passed. exp is 10^i
+    # where i is current digit number
+
+    # The output array elements that will have sorted arr
+    output = copy.deepcopy(list_of_lists)
+
+    for i in range(0, len(output)):
+        for j in range(0, len(output[i])):
+            output[i][j] = 0
+
+    exp = 1
+    while max1 / exp >= 1:
+        n = len(list_1)
+
+        # initialize count array as 0
+        count = [0] * 10
+
+        # Store count of occurrences in count[]
+        for i in range(0, n):
+            index = list_1[i] // exp
+            count[index % 10] += 1
+
+        # Change count[i] so that count[i] now contains actual
+        # position of this digit in output array
+        for i in range(1, 10):
+            count[i] += count[i - 1]
+
+        # Build the output array
+
+        i = n - 1
+        while i >= 0:
+            for j in range(0, len(list_of_lists)):
+                index = list_1[i] // exp
+                output[j][count[index % 10] - 1] = list_of_lists[j][i]
+            count[index % 10] -= 1
+            i -= 1
+
+        # Copying the output array to arr[],
+        # so that arr now contains sorted numbers
+        i = 0
+        for i in range(0, len(list_1)):
+            for j in range(0, len(list_of_lists)):
+                list_of_lists[j][i] = output[j][i]
+        exp *= 10
+
+    # un-100ify them
+    for i in range(0, len(list_1)):
+        list_1[i] = float(list_1[i]) / 100
+
+    # This code is contributed by Mohit Kumra
+    # Edited by Patrick Gallagher
+    # And then altered to make multiple_sort work by Xavier Burt
